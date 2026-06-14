b0e5e9487b8513addce9d252e07ba5da8be8192d
TrungTech <tranquoctrung270302@gmail.com>
2026-06-14 23:16:57 +0700

    add project id

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "project_id" {
   description = "The GCP project ID to deploy resources into."
   type        = string
+  default     = "project-239d8954-80d2-4393-8f6"
 }
 
 variable "region" {
